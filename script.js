32008fbb220e9fe72e172e254e3a0c16ada458bb
parkhomenko <dmitriy.parkhomenko@icloud.com>
2015-11-09 16:22:25 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+angular.module('asideApp', ['ui.bootstrap', 'ngAside'])
+  .controller('MainCtrl', function($scope, $aside) {
+    var asideInstance = $scope.openAside = function openAside(position) {
+      $aside.open({
+        placement: position,
+        templateUrl: 'aside.html',
+        size: 'sm'
+      });
+    };
+  });
